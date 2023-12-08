bf8fe8d54ce8c4b0531f9093c333e32a0e257588
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2023-12-08 00:46:09 -0600

    add gitignore

--- 2023/Challenge-1/index.js
+++ 2023/Challenge-1/index.js
@@ -1,5 +1,8 @@
-function findFirstRepeated(gifts) {  
+const findFirstRepeated = (gifts) => {  
   return parseInt(gifts.filter((element,index) => {
     return (gifts.indexOf(element) !== index) && element
   })[0]) || -1  
 }
+
+
+module.exports = findFirstRepeated
