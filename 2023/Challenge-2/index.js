f6c6ad95a791462f95df8f1ff8a4b35c304a88f3
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-12-08 18:27:57 -0600

    Start code

--- 2023/Challenge-2/index.js
+++ 2023/Challenge-2/index.js
@@ -1,4 +1,14 @@
 function manufacture(gifts, materials) {
-  // Code here
+  let canMade = gifts.map((gift) => {
+    return (
+      gift.split("").every((element) => materials.split("").indexOf(element) ) &&
+      gift
+    );
+  });
+
   return [];
 }
+
+const gifts = ["tren", "oso", "pelota"];
+const materials = "tronesa";
+manufacture(gifts, materials); // ["tren", "oso"]
