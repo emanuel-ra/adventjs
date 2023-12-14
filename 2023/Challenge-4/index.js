a9e153b77a33fc722120ebba0ce80b5e763fea34
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-12-13 18:27:53 -0600

    edited

--- 2023/Challenge-4/index.js
+++ 2023/Challenge-4/index.js
@@ -1,19 +1,16 @@
 function decode(message) {
-  // nesecito mover los strings
-    const newMessage = [...message]
+    
+  const nested = ({ message }) => {   
+    return ''
+  };
+  const noNested = ({ message }) => {   
+    return ''
+  };
+
   return [...message].filter((word) => word === "(").length < 2
-    ? "no nesting"
-    : [...message].map((word, index, arr) => {
-        return word === "("
-          ? arr.slice(arr.indexOf(")"), arr.lastIndexOf(")")).reverse().join('')
-            : word === ")"
-          ? arr.slice(arr.indexOf("("), arr.lastIndexOf("(")).reverse().join('')
-            : word;
-      })
-      .filter(word => word!=='(')
-      .filter(word => word!==')')
-      .join('');
+    ? noNested({ message })
+    : nested({ message });
 }
 
-const c = decode("sa(u(cla)atn)s");
-console.log(c); // santaclaus
+const c = decode('sa(u(cla)atn)s')
+console.log(c) // santaclaus
